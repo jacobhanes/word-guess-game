e54ad80678ac4d45b704a02a7ed56ba473b8a55a
jake hanes <jhanesx@yahoo.com>
2019-06-01 10:27:46 -0700

    changed vars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,15 @@
 let wins = 0;
-let guessed = "";
-let correct = "";
+let trys = 12;
+let guessed = [ ];
+let space = ;
 const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const questions = ["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune","pluto"];
 
+let showLives =
+document.getElementById("remainingGuess");
 
-document.onkeyup = funcion(event) {
-    if (event.key = correctLetter)
-}
+
+//document.onkeyup = funcion(event) {
+  //  if (event.key = correctLetter)
+    //document.getElementById(correctGuess);
+//}
