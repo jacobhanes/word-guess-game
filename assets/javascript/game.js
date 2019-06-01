15872945c8ef06e820716b490572e93fdda270a6
jake hanes <jhanesx@yahoo.com>
2019-05-31 20:05:41 -0700

    added vars jk const/lets

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+let wins = 0;
+let guessed = "";
+let correct = "";
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const questions = ["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune","pluto"];
+
+
+document.onkeyup = funcion(event) {
+    if (event.key = correctLetter)
+}
