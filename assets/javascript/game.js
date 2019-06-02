e0bc6c46c3e04c94f0d16d8569917002e363cb0d
jake hanes <jhanesx@yahoo.com>
2019-06-01 22:59:17 -0700

    created js for picking catagory

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,50 @@
-let wins = 0;
-let trys = 12;
-let guessed = [ ];
-let space = ;
-const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-const questions = ["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune","pluto"];
+window.onload = funcion() {
 
-let showLives =
-document.getElementById("remainingGuess");
+  let wins = 0;
+  let trys = 12;
+  let guessed = [];
+  let space =;
+  let counter;
+  let guess;
+  let chosenWord;
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const questions = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto"];
 
+  let showLives =
+    document.getElementById("remainingGuess");
 
+    //choosing a word
+  var selectCat = function () {
+    if (chosenWord === questions[0]) {
+      Hint.innerHTML = "planet";
+    } else if (chosenCategory === questions[1]) {
+      Hint.innerHTML = "planet";
+    } else if (chosenCategory === questions[2]) {
+      Hint.innerHTML = "planet";
+    }
+  }
+// shows guesses
+  result = function () {
+    wordHolder = document.getElementById('correctGuess');
+    correct = document.createElement('ul');
+
+    for (var i = 0; i < chosenWord.length; i++) {
+      correct.setAttribute('id', 'my-word');
+      guess = document.createElement('li');
+      guess.setAttribute('class', 'guess');
+      if (chosenWord[i] === "-") {
+        guess.innerHTML = "-";
+        space = 1;
+      } else {
+        guess.innerHTML = "_";
+      }
+
+      geusses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
+  }
+}
 //document.onkeyup = funcion(event) {
   //  if (event.key = correctLetter)
     //document.getElementById(correctGuess);
